4d2e60caf4fa7d3411d2e4aac2cf9d004ce1a8b7
JureFilipovic <jure.filipovic@gmail.com>
2025-02-27 13:13:18 +0100

    feat: Add createProject factory to create projects and manage tasks
    Implemented createProject function to create new
    projects. Provides methods for managing tasks within
    the project (add and update tasks).Ensures tasks are
    added only if they don't already exist, and allows
    updating task properties.

---
+++ src/project.js
@@ -0,0 +1,59 @@
+import createTask from "./task.js";
+
+/**
+ * Creates a new project with a given title, where tasks can be added and updated.
+ *
+ * @param {string} title - The title of the project.
+ * @returns {object} An object representing the project with methods to get, set, add, and update tasks.
+ */
+export default function createProject(title) {
+    let _tasks = [];
+    let _title = title;
+
+    // Getter methods
+    const getTasks = () => _tasks;
+    const getTitle = () => _title;
+    
+    // Setter methods
+    const setTitle = (newTitle) => { _title = newTitle; };
+
+    // Adds a task to the _tasks array, returns true if successful and false if not
+    const addTask = (task) => {
+        if (_tasks.find(t => t.getId() === task.getId())) {
+            return false; // Task exists
+        } 
+
+        _tasks.push(task);
+        return true; // Task added successfully
+    }
+
+    // Updates task property, returns true if successful and false if not
+    const updateTask = (taskId, property, newValue) => {
+        const task = _tasks.find(t => t.getId() === taskId);
+        if (!task) return false; // Task doesn't exist
+
+        const propertySetters = {
+            "title": task.setTitle,
+            "description": task.setDescription,
+            "dueDate": task.setDueDate,
+            "notes": task.setNotes,
+            "priority": task.setPriority,
+            "completed": task.toggleCompleted,
+        };
+
+        if (propertySetters[property]) {
+            propertySetters[property](newValue);
+            return true; // Task property set successfully
+        } else {
+            return false; // Task property doesn't exist
+        }
+    }
+
+    return {
+        getTasks,
+        getTitle,
+        setTitle,
+        addTask,
+        updateTask,
+    };
+}
