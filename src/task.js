c1b75112ca5743bd87975106c08152452e65d531
JureFilipovic <jure.filipovic@gmail.com>
2025-02-27 11:47:25 +0100

    Add unique ID to createTask
    Introduced generateId function to generate unique
    sequential IDs for each task. Updated createTask
    to include an auto-generated, immutable ID for
    each task. Exposed getId method to retrieve the
    task ID while keeping it immutable.

--- src/task.js
+++ src/task.js
@@ -1,5 +1,21 @@
+let idCounter = 0; // A counter that tracks the next ID to be generated for tasks.
+
 /**
- * Creates a new task with the given details.
+* Generates a unique ID for each task by incrementing a counter.
+* This ensures that each task gets a unique, sequential ID.
+*
+* @returns {number} The next unique task ID.
+*/
+const generateId = () => {
+	idCounter += 1;
+	return idCounter;
+}
+
+/**
+ * Creates a new task with the provided details and generates a unique ID.
+ * The task includes properties like title, description, due date, notes, and priority,
+ * along with methods for getting and setting these properties.
+ * The ID is generated automatically and cannot be changed after the task is created.
  *
  * @param {string} title - The title of the task.
  * @param {string} description - The description of the task.
@@ -9,6 +25,7 @@
  * @returns {object} An object representing the task, with methods to get and set task properties.
  */
 export default function createTask(title, description, dueDate, notes, priority) {
+	const _id = generateId();
 	let _title = title;
 	let _description = description;
 	let _dueDate = dueDate;
@@ -17,6 +34,7 @@ export default function createTask(title, description, dueDate, notes, priority)
 	let _completed = false;
 
 	// Getter methods
+	const getId = () => _id;
 	const getTitle = () => _title;
 	const getDescription = () => _description;
 	const getDueDate = () => _dueDate;
@@ -33,6 +51,7 @@ export default function createTask(title, description, dueDate, notes, priority)
 	const toggleCompleted = () => { _completed = !_completed; };
 
 	return {
+		getId,
 		getTitle,
 		getDescription,
 		getDueDate,
